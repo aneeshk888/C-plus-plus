dcdb0f080d5a918aa4c5e6f4bfe777931c5cb112
Kutty <kutty@POWERTRON>
2025-07-31 23:17:43 +0530

    Lol

---
+++ max.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int a,b;
+     cout<<"To determine the minimum value of the 2 number"<<endl<<endl;
+     cout<<"Enter the value of a: ";
+     cin>>a;
+     cout<<"Enter the value of b: ";
+     cin>>b;
+    
+    if (a>b){
+        cout<<"a is the maximum value: "<<a<<endl;
+    }
+    else{
+        cout<<"b is the maximum value: "<<b<<endl;
+    }
+}
