9d2b85439f10c7fc1e664e2ba36da9e7c7f5bb1c
Monkey D Luffy <kutty@POWERTRON>
2025-07-31 23:41:39 +0530

    haha

---
+++ prime.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Check if a number is prime" << endl;
+    cout << "Enter the value of N: ";
+    cin >> n;
+
+    if (n <= 1) {
+        cout << "Not a prime number: " << n << endl;
+        return 0;
+    }
+
+    bool isPrime = true;
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) {
+            isPrime = false;
+            break;
+        }
+    }
+
+    if (isPrime)
+        cout << "Prime number: " << n << endl;
+    else
+        cout << "Non-Prime number: " << n << endl;
+
+    return 0;
+}
