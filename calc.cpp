7433276d2c27ba5bf5148c1eff136ef12faf76c6
Alienz <kutty@POWERTRON>
2025-08-02 00:19:32 +0530

    haha

---
+++ calc.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"********Calculator***********"<<endl;
+    cout<<"Enter the A value : ";
+    cin>>a;
+    cout<<"Enter the B value : ";
+    cin>>b;
+
+    cout<<"Addtion = " <<(a+b)<<endl;
+    cout<<"Subtraction = " <<(a-b)<<endl;
+    cout<<"Multiplication = " <<(a*b)<<endl;
+    cout<<"Division = " <<(a/b)<<endl;
+    cout<<"Modulus = " <<(a%b)<<endl;
+}
