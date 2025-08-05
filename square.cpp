eb894b01674b020978f1508592a65e6bcb7b56d4
Alienz <kutty@POWERTRON>
2025-08-05 18:17:45 +0530

    Up

--- square.cpp
+++ square.cpp
@@ -6,11 +6,12 @@ int main(){
     cout<<"Square pattern problem"<<endl;
     cout<<"Enter the N value: ";
     cin>>n;
-    int num=1;
-    for (int i=0;i<n;i++){
-        for (int j=0;j<n;j++){
-            cout<<num<<" ";
-            num++;
+    char ch='A';
+    for (int i=1;i<=n;i++){
+    
+        for (int j=1;j<=n;j++){
+            cout<<ch;
+            ch+=1;
         }
         cout<<endl;
     }
