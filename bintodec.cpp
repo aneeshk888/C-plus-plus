5533a597bf85bca0e462b98db297f2dbba4f0fcc
Alienz <kutty@POWERTRON>
2025-08-07 14:32:29 +0530

    hehe

---
+++ bintodec.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int bintodec(int binnum){
+int ans=0,pow=1;
+
+while(binnum>0){
+
+int rem=binnum%10;
+ans+=rem*pow;
+binnum/=10;
+pow*=2;
+
+}
+return ans;
+}
+
+
+int main(){
+cout<<bintodec(101)<<endl;
+return 0;
+}
